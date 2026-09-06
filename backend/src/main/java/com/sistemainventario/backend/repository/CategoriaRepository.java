097d70734fbe0f22a488ab2208b75f6300b327e0
U23220925 <U23220925@utp.edu.pe>
2026-09-05 19:31:35 -0500

    feat: agregar Productos Repository y Cateforia Repository

---
+++ backend/src/main/java/com/sistemainventario/backend/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.sistemainventario.backend.repository;
+
+import com.sistemainventario.backend.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
